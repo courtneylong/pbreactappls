31d65470a806da7995fc5850c2b1f80687c27225
courtneylong <courtney.long@powerhrg.com>
2023-11-14 11:42:00 -0500

    adding github pages functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Card, Title } from 'playbook-ui'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Card>{ <Title
+          size={4}
+          tag="h4"
+          text="Look, it's a react app"
+      />}</Card>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey! It's a React App!
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'playbook-ui/dist/playbook.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
